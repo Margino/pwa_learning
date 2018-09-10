8b067addf2c2fa9491efe01b5b3692414304216f
Alex Petrov <i@petrov.com.ru>
2018-09-10 20:59:53 +0300

    retrieving from the cache

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,32 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Installing Service Worker ...', e);
+  // use event.waitUntil() because of acync
+  e.waitUntil(
+      // 'static' is just an arbitraty name for the cache
+      caches.open('static')
+        .then((cache) => {
+            console.log('[Service worker] Precaching App');
+            // add files to the cache
+            cache.add('/src/js/app.js')
+        })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
+self.addEventListener('activate', (e) => {
+  console.log('[Service Worker] Activating Service Worker ...', e);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+      caches.match(e.request)
+        .then((response) => {
+            if (response) {
+                // get the cached files
+                return response;
+            } else {
+                return fetch(e.request);
+            }
+        })
+  );
 });
